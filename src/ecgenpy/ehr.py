44f28dd886effa8a1ad7de04c2fc2e1b473e306c
Wai-Shing Luk <luk036@gmail.com>
2022-08-14 11:50:07 +0000

    apply trunk fmt --all

--- src/ecgenpy/ehr.py
+++ src/ecgenpy/ehr.py
@@ -49,7 +49,7 @@ def Ehr_gen(n: int):
             break
         c[k] += 1
         yield b[k]
-        b[1:k] = b[k-1:0:-1]
+        b[1:k] = b[k - 1 : 0 : -1]
 
 
 def Ehr(n: int):
--- tests/test_gray_code.py
+++ tests/test_gray_code.py
@@ -6,28 +6,28 @@ def test_BRGC_gen_odd():
     cnt = 1
     for _ in BRGC_gen(5):
         cnt += 1
-    assert cnt == 2 ** 5
+    assert cnt == 2**5
 
 
 def test_BRGC_gen_even():
     cnt = 1
     for _ in BRGC_gen(6):
         cnt += 1
-    assert cnt == 2 ** 6
+    assert cnt == 2**6
 
 
 def test_BRGC_odd():
     cnt = 0
     for _ in BRGC(5):
         cnt += 1
-    assert cnt == 2 ** 5
+    assert cnt == 2**5
 
 
 def test_BRGC_even():
     cnt = 0
     for _ in BRGC(6):
         cnt += 1
-    assert cnt == 2 ** 6
+    assert cnt == 2**6
 
 
 def test_EMK_gen_odd():
